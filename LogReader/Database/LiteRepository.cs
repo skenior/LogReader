1eb7aaad5fc72d5ef9a298b5230a54c3fca1c59b
Adam Zdunek <zdunekadam@hotmail.com>
2019-10-05 21:03:34 +0200

    Cleaning up

--- LogReader/Database/LiteRepository.cs
+++ LogReader/Database/LiteRepository.cs
@@ -4,9 +4,16 @@ namespace LogReader.Database
 {
   public class LiteRepository<T> : IRepository<T>
   {
+    private readonly string _fileName;
+
+    public LiteRepository(string fileName = "data.db")
+    {
+      _fileName = fileName;
+    }
+
     public void Save(T item)
     {
-      using (var db = new LiteDatabase("data.db"))
+      using (var db = new LiteDatabase(_fileName))
       {
         var collection = db.GetCollection<T>();
         collection.Insert(item);
--- LogReader/Program.cs
+++ LogReader/Program.cs
@@ -2,7 +2,6 @@
 using LogReader.Domain;
 using System;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
@@ -12,25 +11,23 @@ namespace LogReader
   {
     static void Main(string[] args)
     {
-      //if (args.Length != 1)
-      //{
-      //  Console.WriteLine("Incorrect parameters. App requires path to logfile.txt");
-      //  Environment.Exit(-1);
-      //}
-      //var path = args[0];
-      //if (!File.Exists(path))
-      //{
-      //  Console.WriteLine("File was not found.");
-      //  Environment.Exit(-1);
-      //}
-      //var info = new FileInfo(path);
-      //if(info.Name != "logfile.txt")
-      //{
-      //  Console.WriteLine("File is not logfile.txt");
-      //  Environment.Exit(-1);
-      //}
-
-      var path = @"C:\Users\skeni\source\repos\LogReader\LogReader.Tests\Samples\logfile.txt";
+      if (args.Length != 1)
+      {
+        Console.WriteLine("Incorrect parameters. App requires path to logfile.txt");
+        Environment.Exit(-1);
+      }
+      var path = args[0];
+      if (!File.Exists(path))
+      {
+        Console.WriteLine("File was not found.");
+        Environment.Exit(-1);
+      }
+      var info = new FileInfo(path);
+      if (info.Name != "logfile.txt")
+      {
+        Console.WriteLine("File is not logfile.txt");
+        Environment.Exit(-1);
+      }
 
       var dbSave = new DbSaver<EventItem>(new LiteRepository<EventItem>());
       var toSave = new BufferBlock<EventItem>();
